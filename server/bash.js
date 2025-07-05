1cea6a8a72be017a022480bdeb577fc6de3b9a5f
mashaal <omar@teacups.io>
2025-07-05 11:27:34 +1000

    Update bash.js

--- server/bash.js
+++ server/bash.js
@@ -7,22 +7,22 @@ const shaders = [
   "tracking",
   "selective-grayscale",
 ];
+
 let index = 0;
 
-const cwd = "./server/scripts";
+const run = (cmd) => {
+  Deno.run({ cmd: cmd.split(" "), cwd: "./server/scripts" });
+};
 
 export const bashParse = (data) => {
-  if (data?.channel == 8) {
-    console.log("shade");
-    Deno.run({ cmd: ["kitty", "sh", "pixterm.sh"], cwd });
-    Deno.run({
-      cmd: ["hyprshade", "on", `${shaders[index]}.glsl`],
-      cwd,
-    });
-    index = (index + 1) % shaders.length;
-  }
-  if (data?.channel == 11) {
-    console.log("shake");
-    Deno.run({ cmd: ["sh", "shake.sh"], cwd });
+  switch (data?.channel) {
+    case 8:
+      run("kitty sh pixterm.sh");
+      run(`hyprshade on" ${shaders[index]}.glsl`);
+      index = (index + 1) % shaders.length;
+      break;
+    case 11:
+      run("sh shake.sh");
+      break;
   }
 };
